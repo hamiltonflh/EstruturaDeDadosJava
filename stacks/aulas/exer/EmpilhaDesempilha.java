4540d71607272da145d5b8725b8179c54d066962
hamiltonflh <161047380+hamiltonflh@users.noreply.github.com>
2024-09-17 06:57:37 -0300

    Melhor organização dos arquivos

---
+++ stacks/aulas/exer/EmpilhaDesempilha.java
@@ -0,0 +1,38 @@
+package stacks.aulas.exer;
+
+
+import stacks.aulas.Pilha;
+
+import java.util.Random;
+
+public class EmpilhaDesempilha {
+    public static void main(String[] args) throws InterruptedException {
+        Random random = new Random();
+        Pilha<Integer> numeros = new Pilha<>(20);
+
+        for(int i=0;i<=20;i++){
+            int num = random.nextInt(100);
+            if(num%2==0){
+                numeros.empilha(num);
+                System.out.println("Empilhando o número "+ num);
+            } else if (num%2==1) {
+                if(!numeros.estaVazia()){
+                    System.out.println("Número ímpar, desempilhando o número " + numeros.desempilha()+ " da pilha.");
+                } else{
+                    System.out.println("Pilha está vazia");
+                }
+            }
+        }
+
+        if(!numeros.estaVazia()){
+            System.out.println("Desempilhando os números que sobraram...");
+            for(int i=0;i<numeros.tamanho();i++){
+                Thread.sleep(1000);
+                System.out.println(numeros.desempilha());
+            }
+        } else{
+            System.out.println("A pilha terminou vazia");
+        }
+
+    }
+}
---
+++ stacks/aulas/teste/ClassStack.java
@@ -0,0 +1,28 @@
+package stacks.aulas.teste;
+
+import java.util.Stack;
+
+public class ClassStack {
+    public static void main(String[] args){
+        Stack<Integer> stack = new Stack<>();
+
+        System.out.println(stack.isEmpty());
+
+        stack.push(1);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.push(6);
+        stack.push(7);
+
+        System.out.println(stack.isEmpty());
+        System.out.println(stack.size());
+        System.out.println(stack);
+
+        System.out.println(stack.peek());
+
+        System.out.println(stack.pop());
+
+        System.out.println(stack);
+    }
+}
