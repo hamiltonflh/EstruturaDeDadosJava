3ea547b9bad5550aae982ce4139138663b59be41
hamiltonflh <161047380+hamiltonflh@users.noreply.github.com>
2024-09-27 08:44:01 -0300

    Questão 3 corrigida

--- avaliacao/Q3_diagonal_matriz.java
+++ avaliacao/Q3_diagonal_matriz.java
@@ -1,10 +1,13 @@
 package avaliacao;
 
+import arrays.aulas.vetor.estruturas.Vetor;
+
 import java.util.Random;
 
 public class Q3_diagonal_matriz {
     public static void main(String[] args) {
         int [][] matriz = new int[3][3];
+        Vetor<Integer> diagonal_principal = new Vetor<>();
         preencherMatriz(matriz);
 
 
@@ -20,15 +23,16 @@ public class Q3_diagonal_matriz {
 
         System.out.println("Diagonal principal: ");
 
-        diagonal_principal(matriz);
+        diagonal_principal(matriz, diagonal_principal);
+        System.out.println(diagonal_principal.toString());
 
     }
 
-    private static void diagonal_principal(int[][] matriz){
+    private static void diagonal_principal(int[][] matriz, Vetor<Integer> vetor){
         for(int l=0;l<matriz.length;l++){
             for(int c=0;c< matriz.length;c++){
                 if(c==l){
-                    System.out.print("[" + matriz[l][c] + "]");
+                   vetor.adiciona(matriz[l][c]);
                 }
             }
         }
